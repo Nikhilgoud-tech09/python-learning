a3c4e90c84da9add97461c1cc6ed62e45112f638
Nikhilgoud-tech09 <nikhilgoud2127@gmail.com>
2026-01-20 15:52:35 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,15 @@
+#it's strts with _ or letter
+myvar = "nikhl"
+_name = "nikhil"
+print(myvar)
+print(_name)
+
+
+#Create two variables a = 10 and b = 20. Print their sum.
+a = 10
+b = 20
+print(a + b)
+
+#Store your city name in a variable and print:
+city = "Nagpur"
+print("I live in", city)
